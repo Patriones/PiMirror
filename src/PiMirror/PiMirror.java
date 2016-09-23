4061f68aab7037d0d5693b530bcf31d79d127185
Chase Butler <patriones@gmail.com>
2016-09-22 23:48:48 -0400

    Added Test class
    Time +

--- src/PiMirror/PiMirror.java
+++ src/PiMirror/PiMirror.java
@@ -3,13 +3,17 @@ package PiMirror;
 import org.jsoup.*;
 import org.jsoup.nodes.Document;
 import java.io.*;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 public class PiMirror {
 	
-	static String temperature;
-	static String condition;
+	String temperature;
+	String condition;
+	String day, month, year, time;
 	
-	public static void weather() throws IOException {
+	public void weatherTime() throws IOException {
 		Document doc = Jsoup.connect("http://www.accuweather.com/en/us/atlanta-ga/30303/weather-forecast/348181").get(); 
 		String text = doc.text();
 		int i = text.indexOf("Local Weather Today");
@@ -22,13 +26,22 @@ public class PiMirror {
 		i = condition.indexOf(" ");
 		condition = condition.substring(i+1);
 		System.out.println(condition);
+		
+		DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
+	    Date dateobj = new Date();
+	    System.out.println(df.format(dateobj));
+	}
+	
+	
+	public void draw() {
+		
 	}
 	
 	
 	
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		weather();
+		
 	}
 
 }
---
+++ src/PiMirror/Test.java
@@ -0,0 +1,11 @@
+package PiMirror;
+
+import java.io.IOException;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException{
+		PiMirror pi = new PiMirror();
+		pi.weatherTime();
+	}
+}
