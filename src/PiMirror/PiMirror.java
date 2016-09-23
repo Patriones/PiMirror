86ac08f7e12c022ed63df26fea51751c73c450bd
Chase Butler <patriones@gmail.com>
2016-09-23 00:48:38 -0400

    Cleaned up weatherTime method
    Next is actual interface

--- src/PiMirror/PiMirror.java
+++ src/PiMirror/PiMirror.java
@@ -11,25 +11,24 @@ public class PiMirror {
 	
 	String temperature;
 	String condition;
-	String day, month, year, time;
+	String time, date;
 	
 	public void weatherTime() throws IOException {
 		Document doc = Jsoup.connect("http://www.accuweather.com/en/us/atlanta-ga/30303/weather-forecast/348181").get(); 
 		String text = doc.text();
 		int i = text.indexOf("Local Weather Today");
 		temperature = text.substring(i-5, i-1);
-		System.out.println(temperature);
 		i = text.indexOf("See Hourly");
 		condition = text.substring(i-15,i-1);
 		i = condition.indexOf(" ");
 		condition = condition.substring(i+1);
 		i = condition.indexOf(" ");
 		condition = condition.substring(i+1);
-		System.out.println(condition);
-		
+
 		DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm:ss");
 	    Date dateobj = new Date();
-	    System.out.println(df.format(dateobj));
+	    date = df.format(dateobj).substring(0,8);
+	    time = df.format(dateobj).substring(9,17);
 	}
 	
 	
--- src/PiMirror/Test.java
+++ src/PiMirror/Test.java
@@ -7,5 +7,9 @@ public class Test {
 	public static void main(String[] args) throws IOException{
 		PiMirror pi = new PiMirror();
 		pi.weatherTime();
+		System.out.println(pi.date);
+		System.out.println(pi.temperature);
+		System.out.println(pi.time);
+		System.out.println(pi.condition);
 	}
 }
